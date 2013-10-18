4f702c75dd00f11bc74f59c1f724517c335063bc
mjhannaf <mike.hannaford@live.com>
2013-10-18 16:36:27 -0400

    Initial commit

---
+++ Hamming/Hamming/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hamming
+{
+    internal class Program
+    {
+        public static int n;
+        public static List<int> stack = new List<int>();
+
+        private static void Main(string[] args)
+        {
+            // Get the n value
+            Console.Write("Enter n: ");
+            n = int.Parse(Console.ReadLine());
+
+
+            // Get top limit
+            Console.Write("\nEnter power: ");
+            int pow = int.Parse(Console.ReadLine());
+
+
+            var file = new System.IO.StreamWriter(@"C:\Users\mhannaford\Desktop\Hamming.txt");
+            double top = Math.Pow(10, pow);
+            double count = 0;
+
+            for (int i = 1; i <= top; i++)
+            {
+                isHammingNumber(i, 2);
+                if (stack.Count > 0)
+                {
+                    ++count;
+                    file.WriteLine(i);
+                    stack.Clear();
+                }
+            }
+
+            file.WriteLine(count + " elements");
+            file.Close();
+        }
+
+        public static void isHammingNumber(int num, int divisor)
+        {
+            if (num == 1)
+            {
+                stack.Add(num);
+            }
+            if (num%divisor == 0)
+            {
+                if (num <= n)
+                {
+                    stack.Add(divisor);
+                }
+                isHammingNumber(num/divisor, 2);
+                return;
+            }
+            if (divisor < n)
+            {
+                isHammingNumber(num, ++divisor);
+            }
+        }
+    }
+}
